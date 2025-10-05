ed09cdb0546fba2d579b8c8ad301a67577286a7d
munevvertopcu <munevvertopcu98@gmail.com>
2025-10-05 20:33:03 +0300

    stuff

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import express from 'express'; // Imports Express.js.
+import express from 'express';
+import reminderRoutes from './routes/remindersRoutes.js';
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware.js';
 
-const app = express() // Creates an Express application instance. app variable is a server. Will accept users and respond to their requests.
-const port = process.env.PORT || 3000;
+const app = express() // Creates an Express application instance.
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-}) // tells us what to do when the user requests something with GET. If someone comes to our homepage with a GET request (/), send them 'Hello World!'.
+app.use(express.json())
+app.use('/reminders', reminderRoutes)
 
-app.get('/profile', (req, res) => {
-  res.send('Hello developer!')
-})
+app.use(errorHandlerMiddleware)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-}) // Starts the server on port 3000 and logs a confirmation message. Now the server is constantly listening for that door.
+})
